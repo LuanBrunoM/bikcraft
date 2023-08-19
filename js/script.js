035a95c5aeafc539c06de92a940d5a00c02061de
Luan Bruno <luanbrunomelo03@gmail.com>
2023-08-19 17:56:07 -0300

    active menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log('teste')
+const links = document.querySelectorAll('.header-menu a');
+
+function activateLinks(link) {
+  const url = location.href
+  const href = link.href
+
+  
+  if(url.includes(href)) {
+    link.classList.add('active');
+  }
+}
+
+links.forEach(activateLinks)
